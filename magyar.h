3dd075f3ebe8071c35ce9979be62df2509de5017
kutyamutya <budosgane@gmail.com>
2015-02-25 14:18:25 +0100

    First real commit

---
+++ magyar.h
@@ -0,0 +1,10 @@
+#define egesz int
+#define visszateres return
+#define fofuggveny(...) main(__VA_ARGS__)
+#define kiiratas(...) printf( __VA_ARGS__)
+#define karakter char
+#define ha(...) if( __VA_ARGS__ )
+#define maskulonben else
+#define NULLA 0
+
+//if else switch case default break goto return for while do continue typedef sizeof NULL
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "magyar.h"
+
+egesz fofuggveny(egesz argc, karakter** argv){
+  egesz i = 43;
+  ha(i <40) {
+   //
+  } maskulonben {
+  
+  }
+  kiiratas("valami %d \n", i);
+  visszateres 0;
+}
